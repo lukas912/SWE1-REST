d44d391e8d8ed00b3ecd44b92858936bb0769690
Lukas Nowy <lukas.nowy@gmail.com>
2020-10-29 14:27:49 +0100

    HttpServer created

---
+++ RestServer/RestServer/HttpServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace RestServer
+{
+    class HttpServer
+    {
+        public const string VERSION = "HTTP/1.1";
+        public const string NAME = "MyRestServer v0.1";
+        public const string dir_err = "/data/err";
+        public const string dir_html = "/data/html";
+        public const string dir_json = "/data/json";
+        public const string dir_plaintext = "/data/text";
+        public const string dir_img = "/data/img";
+        bool is_running = false;
+        TcpListener listener;
+
+
+        public HttpServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public void startServer()
+        {
+            Thread st = new Thread(new ThreadStart(runServer));
+            st.Start();
+            Console.WriteLine("Server started!");
+        }
+
+        private void runServer()
+        {
+            is_running = true;
+            listener.Start();
+
+            while (is_running)
+            {
+                Console.WriteLine("Waiting for connection ...");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected!");
+                HandleClient(client);
+                client.Close();
+            }
+
+
+            listener.Stop();
+            is_running = false;
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            string data = "";
+
+            while (reader.Peek() != -1)
+            {
+                data += reader.ReadLine();
+            }
+
+            //Console.Write(data);
+
+
+        }
+    }
+}
---
+++ RestServer/RestServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Server ...");
+            HttpServer server = new HttpServer(7000);
+            server.startServer();
+        }
+    }
+}
