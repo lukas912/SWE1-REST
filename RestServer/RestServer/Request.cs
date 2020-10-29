07bf4c74578f3805e99b9a01970a78c949d57f85
Lukas Nowy <lukas.nowy@gmail.com>
2020-10-29 14:31:41 +0100

    Request Class added, Extract Queries from url

---
+++ RestServer/RestServer/Request.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestServer
+{
+    class Request
+    {
+        public string httpverb { get; set; }
+        public string url { get; set; }
+        public string host { get; set; }
+        public List<Query> queries;
+
+        public Request(string httpverb, string url, string host, List<Query> q)
+        {
+            this.httpverb = httpverb;
+            this.url = url;
+            this.host = host;
+            this.queries = q;
+        }
+
+        public static Request GetRequest(string req)
+        {
+            if (String.IsNullOrEmpty(req))
+            {
+                return null;
+            }
+
+            string[] tokens = req.Split(" ");
+            string httpverb = tokens[0];
+            string url = tokens[1];
+            string host = tokens[4];
+            List<Query> queries = getQueries(url);
+
+
+            Console.WriteLine("Request from {0}: {1} {2}", host, httpverb, url);
+
+            return new Request(httpverb, url, host, queries);
+        }
+
+        public static List<Query> getQueries(string url)
+        {
+            List<Query> output = new List<Query>();
+
+            if (url.Contains("?") == false)
+            {
+                return null;
+            }
+
+            else
+            {
+                string[] a = url.Split("?");
+
+                string[] b = a[1].Split("&&");
+
+                foreach (string item in b)
+                {
+                    string[] c = item.Split("=");
+                    Query q = new Query(c[0], c[1]);
+                    output.Add(q);
+                }
+
+            }
+
+            return output;
+
+        }
+    }
+}
